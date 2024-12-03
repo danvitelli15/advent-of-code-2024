ca0c48586ee3990d1b844a36e20ab3a6af087938
Daniel Vitelli <danvitelli15@gmail.com>
2024-12-03 12:47:23 -0600

    even more boiler plate

--- add-day.ts
+++ add-day.ts
@@ -14,7 +14,13 @@ Deno.writeTextFileSync(`${dayDir}/readme.md`, description || "");
 Deno.writeFileSync(`${dayDir}/input.txt`, encoder.encode("input text here"));
 Deno.writeFileSync(
   `${dayDir}/solution.ts`,
-  encoder.encode(`export const solve = (input: string) => {
+  encoder.encode(`export const solvePart1 = (input: string) => {
+  const output = "";
+  console.log(input);
+  return output;
+};
+
+export const solvePart2 = (input: string) => {
   const output = "";
   console.log(input);
   return output;
@@ -22,13 +28,16 @@ Deno.writeFileSync(
 );
 Deno.writeFileSync(
   `${dayDir}/main.ts`,
-  encoder.encode(`import { solve } from './solution.ts';
+  encoder.encode(`import { solvePart1, solvePart2 } from './solution.ts';
     
 const main = () => {
   const encoder = new TextEncoder();
   const input = Deno.readTextFileSync(\`${import.meta.dirname}/input.txt\`);
-  const output = solve(input);
-  Deno.writeFileSync(\`${import.meta.dirname}/output.txt\`, encoder.encode(output));
+  const outputPart1 = solvePart1(input);
+  const outputPart2 = solvePart2(input);
+  Deno.writeFileSync(\`${
+    import.meta.dirname
+  }/output.txt\`, encoder.encode(\`Output Part 1: \${outputPart1}\\nOutput Part 2: \${outputPart2}\`));
 }
 
 main();`)
@@ -38,12 +47,16 @@ Deno.writeFileSync(
   `${dayDir}/test.ts`,
   encoder.encode(
     `import { assertEquals } from '@std/assert';
-import { solve } from './solution.ts';
+import { solvePart1, solvePart2 } from './solution.ts';
 
 const testInput = \`\`;
 
-Deno.test('Day ${day}', () => {
-  assertEquals(solve(testInput), 'expected output');
+Deno.test('Day ${day} - Part 1', () => {
+  assertEquals(solvePart1(testInput), 'expected output');
+});
+
+Deno.test('Day ${day} - Part 2', () => {
+  assertEquals(solvePart2(testInput), 'expected output');
 });`
   )
 );
