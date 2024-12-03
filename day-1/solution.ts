ff2a11dddb3eef94a569e9e36bb26c35bf496d73
Daniel Vitelli <danvitelli15@gmail.com>
2024-12-03 12:39:50 -0600

    solved day 1 part 1

--- day-1/solution.ts
+++ day-1/solution.ts
@@ -1,5 +1,17 @@
 export const solve = (input: string) => {
-  const output = "";
-  console.log(input);
-  return output;
+  const inputRows = input.split("\n");
+  const [left, right] = inputRows.reduce(
+    ([left, right], row) => {
+      const [l, r] = row.split("   ").map((n) => parseInt(n));
+      return [
+        [...left, l],
+        [...right, r],
+      ];
+    },
+    [new Array<number>(), new Array<number>()]
+  );
+  left.sort((a, b) => a - b);
+  right.sort();
+  const sumDifference = left.reduce((sum, l, index) => sum + Math.abs(l - right[index]), 0);
+  return sumDifference.toString();
 };
