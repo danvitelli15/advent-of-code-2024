50d8f8713a8ea20e22ab1fe60329373f4cd29b1a
Daniel Vitelli <danvitelli15@gmail.com>
2024-12-03 22:28:38 -0600

    completed 2-1

--- day-2/solution.ts
+++ day-2/solution.ts
@@ -1,11 +1,52 @@
+const parseReports = (input: string) => {
+  const reports = input.split("\n");
+  const reportValues = reports.map((r) => r.split(" "));
+  const reportValuesAsNumbers = reportValues.map((r) => r.map((n) => parseInt(n)));
+  return reportValuesAsNumbers;
+};
+
+export const reportIsSafe = (report: number[]) => {
+  let isSafe = true;
+
+  report.reduce((previousDifference, value, index, report) => {
+    if (index + 1 < report.length) {
+      const next = report[index + 1];
+      const difference = value - next;
+
+      // if the previous change was an increase and the current change is a decrease
+      // or vice versa, the mutiplication will result in a negative number
+      if (previousDifference * difference < 0) {
+        isSafe = false;
+      }
+
+      const absoluteDiffernece = Math.abs(difference);
+      if (absoluteDiffernece <= 0 || absoluteDiffernece > 3) {
+        isSafe = false;
+      }
+
+      return difference;
+    }
+    return previousDifference;
+  }, 0);
+
+  return isSafe;
+};
+
 export const solvePart1 = (input: string) => {
-  const output = "";
-  console.log(input);
-  return output;
+  const reports = parseReports(input);
+
+  const safeCount = reports.reduce((safeCount, report) => {
+    if (reportIsSafe(report)) {
+      return (safeCount += 1);
+    } else {
+      return safeCount;
+    }
+  }, 0);
+
+  return safeCount.toString();
 };
 
 export const solvePart2 = (input: string) => {
-  const output = "";
-  console.log(input);
-  return output;
+  const reports = parseReports(input);
+  return "output";
 };
--- day-2/test.ts
+++ day-2/test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "@std/assert";
-import { solvePart1, solvePart2 } from "./solution.ts";
+import { reportIsSafe, solvePart1, solvePart2 } from "./solution.ts";
 
 const testInput = `7 6 4 2 1
 1 2 7 8 9
@@ -8,8 +8,32 @@ const testInput = `7 6 4 2 1
 8 6 4 4 1
 1 3 6 7 9`;
 
-Deno.test("Day 2 - Part 1", () => {
+Deno.test("Day 2 - Part 1 - should be 2 safe records", () => {
   assertEquals(solvePart1(testInput), "2");
+});
+
+Deno.test("Day 2 - Part 1 - Records all decrease by 1 or 2", () => {
+  assertEquals(reportIsSafe([7, 6, 4, 2, 1]), true);
+});
+
+Deno.test("Day 2 - Part 1 - increase of more than 3", () => {
+  assertEquals(reportIsSafe([1, 2, 7, 8, 9]), false);
+});
+
+Deno.test("Day 2 - Part 1 - decrease of more than 3", () => {
+  assertEquals(reportIsSafe([9, 7, 6, 2, 1]), false);
+});
+
+Deno.test("Day 2 - Part 1 - both increase and decrease", () => {
+  assertEquals(reportIsSafe([1, 3, 2, 4, 5]), false);
+});
+
+Deno.test("Day 2 - Part 1 - unchanged value", () => {
+  assertEquals(reportIsSafe([8, 6, 4, 4, 1]), false);
+});
+
+Deno.test("Day 2 - Part 1 - Records all increase by 1, 2, or 3", () => {
+  assertEquals(reportIsSafe([1, 3, 6, 7, 9]), true);
 });
 
 Deno.test("Day 2 - Part 2", () => {
