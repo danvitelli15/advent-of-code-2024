5b6a4c306d95991a7dabe4c2589f90493843ca67
Daniel Vitelli <danvitelli15@gmail.com>
2024-12-04 01:57:19 -0600

    completed day 3

--- day-3/solution.ts
+++ day-3/solution.ts
@@ -1,11 +1,86 @@
 export const solvePart1 = (input: string) => {
-  const output = "";
-  console.log(input);
-  return output;
+  const validCommandRegex = /mul\([0-9]{1,3},[0-9]{1,3}\)/g;
+
+  const matches = input.match(validCommandRegex);
+
+  if (!matches) {
+    return "0";
+  }
+
+  const resultTotal = matches.reduce((total, match) => {
+    const numbers = match.match(/[0-9]{1,3}/g);
+    if (!numbers) {
+      return total;
+    }
+    const [a, b] = numbers;
+    return total + parseInt(a) * parseInt(b);
+  }, 0);
+
+  return resultTotal.toString();
 };
 
+// export const solvePart2 = (input: string) => {
+//   const validMulCommandRegex = /mul\([0-9]{1,3},[0-9]{1,3}\)/g;
+//   const enabledStretchesRegex = /(^|do\(\))(.*?)(don't\(\)|$)/g;
+
+//   const enabledStretches = input.match(enabledStretchesRegex);
+
+//   if (!enabledStretches) {
+//     return "0";
+//   }
+
+//   console.log(enabledStretches);
+
+//   const enabledCommands = "".concat(...enabledStretches);
+
+//   console.log(enabledCommands);
+
+//   const matches = enabledCommands.match(validMulCommandRegex);
+
+//   if (!matches) {
+//     return "0";
+//   }
+
+//   console.log(matches);
+
+//   const resultTotal = matches.reduce((total, match) => {
+//     const numbers = match.match(/[0-9]{1,3}/g);
+//     if (!numbers) {
+//       return total;
+//     }
+//     const [a, b] = numbers;
+//     return total + parseInt(a) * parseInt(b);
+//   }, 0);
+
+//   return resultTotal.toString();
+// };
+
 export const solvePart2 = (input: string) => {
-  const output = "";
-  console.log(input);
-  return output;
+  const validMulCommandRegex = /mul\([0-9]{1,3},[0-9]{1,3}\)/g;
+
+  const splitOnDos = input.split("do()");
+
+  const enabledStretches = splitOnDos.map((stretch) => {
+    const splitOnDonts = stretch.split("don't()");
+    return splitOnDonts[0];
+  });
+
+  const enabledCommands = enabledStretches.join();
+
+  const matches = enabledCommands.match(validMulCommandRegex);
+
+  if (!matches) {
+    return "0";
+  }
+
+  const resultTotal = matches.reduce((total, match) => {
+    const numbers = match.match(/[0-9]{1,3}/g);
+    if (!numbers) {
+      return total;
+    }
+    const [a, b] = numbers;
+    return total + parseInt(a) * parseInt(b);
+  }, 0);
+
+  return resultTotal.toString();
 };
--- day-3/test.ts
+++ day-3/test.ts
@@ -1,12 +1,12 @@
-import { assertEquals } from '@std/assert';
-import { solvePart1, solvePart2 } from './solution.ts';
+import { assertEquals } from "@std/assert";
+import { solvePart1, solvePart2 } from "./solution.ts";
 
-const testInput = ``;
+const testInput = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
 
-Deno.test('Day 3 - Part 1', () => {
-  assertEquals(solvePart1(testInput), 'expected output');
+Deno.test("Day 3 - Part 1", () => {
+  assertEquals(solvePart1(testInput), "161");
 });
 
-Deno.test('Day 3 - Part 2', () => {
-  assertEquals(solvePart2(testInput), 'expected output');
+Deno.test("Day 3 - Part 2", () => {
+  assertEquals(solvePart2(testInput), "48");
 });
