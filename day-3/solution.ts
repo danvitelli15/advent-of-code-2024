9c59ca30fa8a7d889032306ea2ff30d94fccd0ea
Daniel Vitelli <danvitelli15@gmail.com>
2024-12-04 00:48:22 -0600

    generated day 3

---
+++ day-3/solution.ts
@@ -0,0 +1,11 @@
+export const solvePart1 = (input: string) => {
+  const output = "";
+  console.log(input);
+  return output;
+};
+
+export const solvePart2 = (input: string) => {
+  const output = "";
+  console.log(input);
+  return output;
+};
---
+++ day-3/test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from '@std/assert';
+import { solvePart1, solvePart2 } from './solution.ts';
+
+const testInput = ``;
+
+Deno.test('Day 3 - Part 1', () => {
+  assertEquals(solvePart1(testInput), 'expected output');
+});
+
+Deno.test('Day 3 - Part 2', () => {
+  assertEquals(solvePart2(testInput), 'expected output');
+});
